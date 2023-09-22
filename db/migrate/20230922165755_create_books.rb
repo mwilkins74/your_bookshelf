96de5f8d7a4081410d24faba7b337f0551db1181
Mark <markwilkins86@gmail.com>
2023-09-22 13:00:01 -0400

    book setup

---
+++ db/migrate/20230922165755_create_books.rb
@@ -0,0 +1,14 @@
+class CreateBooks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.text :description
+      t.integer :number_of_pages
+
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
