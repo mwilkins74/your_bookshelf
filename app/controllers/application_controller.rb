809428ecf99f4e0b417300cad0893c8fca275e34
Mark <markwilkins86@gmail.com>
2023-09-25 10:31:48 -0400

    navbar buttons removed on log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,24 +3,24 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user
 
   helper_method :current_user
-  helper_method :logged_in?
+  helper_method :user_logged_in?
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  def logged_in?
+  def user_logged_in?
     !current_user.nil?
   end
 
   def authorized
-    redirect_to root_path unless logged_in?
+    redirect_to root_path unless user_logged_in?
   end
 
   private
 
   def authenticate_user
-    unless logged_in? || sessions_controller_actions.include?(params[:action]) || request.path == root_path
+    unless user_logged_in? || sessions_controller_actions.include?(params[:action]) || request.path == root_path
       redirect_to root_path, alert: "Please log in to access this page."
     end
   end
